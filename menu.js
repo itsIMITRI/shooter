af2ce9d4e94e17169cb0442b16fdc5338868279c
Imre Varga <vargaimi990330@gmail.com>
2021-08-05 15:52:05 +0200

    Menu button skeleton

--- menu.js
+++ menu.js
@@ -1,10 +1,9 @@
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-// Get the texture for rope.
 const starTexture = PIXI.Texture.from('./star.png');
 
-const starAmount = 500;
+const starAmount = 300;
 let cameraZ = 0;
 const fov = 20;
 const baseSpeed = 0.3;
@@ -13,7 +12,6 @@ let warpSpeed = 0;
 const starStretch = 0.01;
 const starBaseSize = 0.01;
 
-// Create the stars
 const stars = [];
 for (let i = 0; i < starAmount; i++) {
   const star = {
@@ -34,25 +32,19 @@ function randomizeStar(star, initial) {
     ? Math.random() * 2000
     : cameraZ + Math.random() * 1000 + 2000;
 
-  // Calculate star positions with radial random coordinate so no star hits the camera.
   const deg = Math.random() * Math.PI * 2;
   const distance = Math.random() * 50 + 1;
   star.x = Math.cos(deg) * distance;
   star.y = Math.sin(deg) * distance;
 }
 
-// Change flight speed every 5 seconds
-
-// Listen for animate update
 app.ticker.add((delta) => {
-  // Simple easing. This should be changed to proper easing function when used for real.
   speed += (warpSpeed - speed) / 20;
   cameraZ += delta * 10 * (speed + baseSpeed);
   for (let i = 0; i < starAmount; i++) {
     const star = stars[i];
     if (star.z < cameraZ) randomizeStar(star);
 
-    // Map star 3d position to 2d with really simple projection
     const z = star.z - cameraZ;
     star.sprite.x =
       star.x * (fov / z) * app.renderer.screen.width +
@@ -61,18 +53,57 @@ app.ticker.add((delta) => {
       star.y * (fov / z) * app.renderer.screen.width +
       app.renderer.screen.height / 2;
 
-    // Calculate star scale & rotation.
     const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
     const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
     const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
     const distanceScale = Math.max(0, (2000 - z) / 2000);
     star.sprite.scale.x = distanceScale * starBaseSize;
-    // Star is looking towards center so that y axis is towards center.
-    // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
+
     star.sprite.scale.y =
       distanceScale * starBaseSize +
       (distanceScale * speed * starStretch * distanceCenter) /
         app.renderer.screen.width;
     star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+  }
+
+  const style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: 'round',
+  });
+
+  const richText = new PIXI.Text('Bob the Space-Jammer', style);
+  richText.x = 200;
+  richText.y = 50;
+
+  app.stage.addChild(richText);
+
+  const textureButton = PIXI.Texture.from('playbutton.png');
+
+  const button = new PIXI.Sprite(textureButton);
+
+  button.x = 300;
+  button.y = 150;
+  button.scale.x = 0.5;
+  button.scale.y = 0.5;
+
+  app.stage.addChild(button);
+
+  function onButtonDown() {
+    this.isdown = true;
+    this.texture = textureButtonDown;
+    this.alpha = 1;
   }
 });
